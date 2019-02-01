0af80896f3170bb4531fecee351fb9f97822c604
cheikhouna <cheikhounagueye4@gmail.com>
2019-02-01 09:05:42 +0000

    Getting Data Back

--- models/server_db.js
+++ models/server_db.js
@@ -6,7 +6,16 @@ save_user_information = (data) => new Promise((resolve, reject)=>{
     if(err){
       reject('could not insert into lottery information');
     }
-    resolve('Successful')
+    resolve('Successful');
+  });
+})
+
+get_total_amount = (data) => new Promise((resolve, reject)=>{
+  db.query('SELECT sum(amount) as total_amount FROM lottery_information',null, function(err,results, fields){
+    if(err){
+      reject('could not get total amount');
+    }
+    resolve(results);
   });
 })
 
--- server.js
+++ server.js
@@ -20,6 +20,12 @@ var result = await save_user_information({"amount": amount, "email": email })
   res.send(result);
 });
 
+app.get('/get_total_amount', async (req,res)=>{
+
+  var result = await get_total_amount();
+    res.send(result);
+});
+
 app.listen(3000,()=>{
   console.log('server is running on port 3000');
 });
